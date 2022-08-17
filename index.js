89e7d48b8b54562416e6ba2f45790f8a250bdb66
linus mutugi mate <mlynux@gmail.com>
2022-08-17 12:16:00 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+function saturdayFun(newActiviy = "roller-skate"){
+    
+    return `This Saturday, I want to ${newActiviy}!`;
+}
+saturdayFun();
+
+function mondayWork(jobTask = "go to the office"){
+    return `This Monday, I will ${jobTask}.`;
+
+}
+mondayWork();
+
+
+function wrapAdjective(star = "*"){
+    return function (moon = "behavior"){
+        return `You are ${star}${moon}${star}!`;
+    }
+
+}
+
